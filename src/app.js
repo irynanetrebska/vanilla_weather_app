9deb501325241d85cb7778a619dfe9da72da9069
irynanetrebska <irina.netrebska@gmail.com>
2023-04-17 21:40:57 +0200

    updated the time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = date.getDay();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${days[day]} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperature = document.querySelector("#temp-value");
@@ -10,6 +33,8 @@ function displayTemperature(response) {
   humidityValue.innerHTML = response.data.temperature.humidity;
   let windValue = document.querySelector(".wind-value");
   windValue.innerHTML = Math.round(response.data.wind.speed);
+  let dayTimeElement = document.querySelector(".day");
+  dayTimeElement.innerHTML = formatDate(response.data.time * 1000);
 }
 
 let apiKey = "a339ota3fb01500d5581a0bb40c20254";
