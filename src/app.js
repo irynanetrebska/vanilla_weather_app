b7a6915b4225edb8689e27ea6246837c3f46c1bd
irynanetrebska <irina.netrebska@gmail.com>
2023-04-21 22:21:19 +0200

    Implemented the unit conversion

--- src/app.js
+++ src/app.js
@@ -31,7 +31,9 @@ function displayTemperature(response) {
   let iconElement = document.querySelector("#icon");
   let iconCode = response.data.condition.icon;
 
-  temperature.innerHTML = Math.round(response.data.temperature.current);
+  celciusTemperature = Math.round(response.data.temperature.current);
+
+  temperature.innerHTML = celciusTemperature;
   h1.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityValue.innerHTML = response.data.temperature.humidity;
@@ -48,17 +50,49 @@ function search(cityDisplay) {
   let apiKey = "a339ota3fb01500d5581a0bb40c20254";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityDisplay}&key=${apiKey}&units=metric`;
 
-  console.log(apiUrl);
   axios.get(apiUrl).then(displayTemperature);
 }
 
 function handleSubmit(event) {
   event.preventDefault();
+  celciusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
   let cityInputElement = document.querySelector("#city-input");
-  search(cityInputElement.value);
+  let searchedCity = cityInputElement.value.trim();
+  search(searchedCity);
 }
 
 search("Talence");
 
+function convertToFahrehheit(event) {
+  event.preventDefault();
+  celciusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let cityDisplay = document.querySelector("#city");
+  let apiKey = "a339ota3fb01500d5581a0bb40c20254";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityDisplay.innerHTML}&key=${apiKey}&units=imperial`;
+
+  axios.get(apiUrl).then(function (response) {
+    let farenheitValue = document.querySelector("#temp-value");
+    farenheitValue.innerHTML = Math.round(response.data.temperature.current);
+  });
+}
+
+function convertToCelcius(event) {
+  event.preventDefault();
+  celciusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperature = document.querySelector("#temp-value");
+  temperature.innerHTML = celciusTemperature;
+}
+
+let celciusTemperature = null;
+
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", convertToFahrehheit);
+
+let celciusLink = document.querySelector("#celcius-link");
+celciusLink.addEventListener("click", convertToCelcius);
